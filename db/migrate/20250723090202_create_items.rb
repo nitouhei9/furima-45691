464ddfc7c6ca0c4502c9fc2517f85c2b55ad510e
nitouhei9 <kawachan.genmame@gmail.com>
2025-07-25 16:55:36 +0900

    null: false 追加

--- db/migrate/20250723090202_create_items.rb
+++ db/migrate/20250723090202_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.integer :category_id
-      t.integer :price
+      t.string  :name, null: false
+      t.text :description, null: false
+      t.integer :category_id, null: false
+      t.integer :price, null: false
       t.integer :shipping_fee_payer_id, null: false
       t.integer :shipping_region_id, null: false
       t.integer :shipping_day_id, null: false
