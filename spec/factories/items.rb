84c2edf6c4a31df8ca95aab876807d68b429ab9b
nitouhei9 <kawachan.genmame@gmail.com>
2025-07-25 15:59:36 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -11,7 +11,7 @@ FactoryBot.define do
     association :user
 
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      item.image.attach(io: StringIO.new("dummy"), filename: 'test_image.png', content_type: 'image/png')
     end
   end
 end
