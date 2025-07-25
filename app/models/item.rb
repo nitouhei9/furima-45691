864433ab5100122151deb62353c3ca83e1398db2
nitouhei9 <kawachan.genmame@gmail.com>
2025-07-25 16:43:54 +0900

    notnull制約

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,6 +21,7 @@ class Item < ApplicationRecord
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  # アソシエーション
-  has_one :order
+  # # アソシエーション
+  # has_one :order
+  
 end
--- db/migrate/20250723090202_create_items.rb
+++ db/migrate/20250723090202_create_items.rb
@@ -5,10 +5,10 @@ class CreateItems < ActiveRecord::Migration[7.1]
       t.text :description
       t.integer :category_id
       t.integer :price
-      t.integer :shipping_fee_payer_id
-      t.integer :shipping_region_id
-      t.integer :shipping_day_id
-      t.integer :status_id
+      t.integer :shipping_fee_payer_id, null: false
+      t.integer :shipping_region_id, null: false
+      t.integer :shipping_day_id, null: false
+      t.integer :status_id, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
