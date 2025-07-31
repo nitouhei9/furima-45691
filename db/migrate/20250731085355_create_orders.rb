b9dc101d991081225ccc48f7ffe1e1e6d23e69e5
nitouhei9 <kawachan.genmame@gmail.com>
2025-07-31 17:57:11 +0900

    マイグレーションファイルの編集

--- db/migrate/20250731085355_create_orders.rb
+++ db/migrate/20250731085355_create_orders.rb
@@ -3,8 +3,8 @@ class CreateOrders < ActiveRecord::Migration[7.1]
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
-
       t.timestamps
     end
   end
 end
+
--- db/migrate/20250731085401_create_addresses.rb
+++ db/migrate/20250731085401_create_addresses.rb
@@ -1,14 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :addresses do |t|
-      t.string :postal_code
-      t.integer :shipping_region_id
-      t.string :city
-      t.string :street
-      t.string :building
-      t.string :phone_number
-      t.references :order, null: false, foreign_key: true
-
+      t.string     :postal_code,       null: false
+      t.integer    :shipping_region_id, null: false
+      t.string     :city,              null: false
+      t.string     :street,            null: false
+      t.string     :building
+      t.string     :phone_number,      null: false
+      t.references :order,             null: false, foreign_key: true
       t.timestamps
     end
   end
