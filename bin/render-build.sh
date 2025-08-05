8f24d8b6b2098bc698977d8af9bebc7614cb0f36
nitouhei9 <kawachan.genmame@gmail.com>
2025-08-05 12:28:11 +0900

    もとに戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-
-# 本番環境でマイグレーション実行
-RAILS_ENV=production bundle exec rake db:migrate
+bundle exec rake db:migrate
