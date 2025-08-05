1f5ff80fc8ccddd25d48b9d815d37d7f540dde0e
nitouhei9 <kawachan.genmame@gmail.com>
2025-08-05 11:55:14 +0900

    アプリケーションコントローラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
+  # CSRF対策
+  protect_from_forgery with: :exception
+  
+  # Deviseのパラメータ許可
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+    # 会員登録時に許可するパラメータ
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+    ])
+    
+    # アカウント更新時に許可するパラメータ
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+    ])
   end
 end
