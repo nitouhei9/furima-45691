ceef2f0f280d1d33e7fff72af458090f1b903514
nitouhei9 <kawachan.genmame@gmail.com>
2025-08-05 15:57:29 +0900

    basic認証復旧

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Basic認証
+  before_action :basic_auth
+  
   # CSRF対策
   protect_from_forgery with: :exception
   
@@ -6,6 +9,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
+
 
   def configure_permitted_parameters
     # 会員登録時に許可するパラメータ
